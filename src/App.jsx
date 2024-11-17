ee13121f1201bcaa68a3efd0b9282b3871719939
Bara Kona <bkonateh1@gmail.com>
2024-11-17 23:59:01 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,52 @@
-
-import { Route, Routes } from 'react-router-dom';
-import Home from './Components/Pages/Home';
-import Exhibition from './Components/Pages/Exhibition';
-import Artworks from './Components/Pages/Artworks';
-import Sidebar from './Components/Navigation/Sidebar';
-import Grid from '@mui/material/Grid2';
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import Exhibition from "./Components/Pages/Exhibition";
+import Artworks from "./Components/Pages/Artworks";
+import Sidebar from "./Components/Navigation/Sidebar";
+import Grid from "@mui/material/Grid2";
 import { Outlet } from "react-router-dom";
-import { useState, useEffect } from 'react'
-import TopMenu from './Components/Navigation/TopBar';
+import { useState } from "react";
+import TopMenu from "./Components/Navigation/TopBar";
 
 const App = () => {
-  const [selectedCategories, setSelectedCategories] = useState([]); 
-  const [selectedMuseums, setSelectedMuseums] = useState([]); 
-  
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [selectedMuseums, setSelectedMuseums] = useState([]);
+
   return (
-<>
-<Grid container sx={{ width: '100%', zIndex: 1300 }}>
-         <Routes>
-      <Route
-            element={<>
-            
-                    <Sidebar selectedCategories={selectedCategories} setSelectedCategories={setSelectedCategories} selectedMuseums={selectedMuseums} setSelectedMuseums={setSelectedMuseums}/>
-                    <TopMenu/>
+    <>
+      <Grid container sx={{ width: "100%", zIndex: 1300 }}>
+        <Routes>
+          <Route
+            element={
+              <>
+                <Sidebar
+                  selectedCategories={selectedCategories}
+                  setSelectedCategories={setSelectedCategories}
+                  selectedMuseums={selectedMuseums}
+                  setSelectedMuseums={setSelectedMuseums}
+                />
+                <TopMenu />
                 <Outlet />
-                </>
+              </>
             }
           >
- 
-            <Route path="/" element={<Home/>} />
-            <Route path="/exhibition" element={<Exhibition/>} />
-            <Route path="/artworks" element={<Artworks selectedCategories={selectedCategories} selectedMuseums={selectedMuseums}/>} />
-            <Route path="/home" element={<Home/>} />
-            </Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/exhibition" element={<Exhibition />} />
+            <Route
+              path="/artworks"
+              element={
+                <Artworks
+                  selectedCategories={selectedCategories}
+                  selectedMuseums={selectedMuseums}
+                />
+              }
+            />
+            <Route path="/home" element={<Home />} />
+          </Route>
         </Routes>
-        </Grid>
-        </>
-);
+      </Grid>
+    </>
+  );
 };
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from '@mui/material/styles';
-import { appTheme } from './appTheme.js';
+import { ThemeProvider } from "@mui/material/styles";
+import { appTheme } from "./appTheme";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <ThemeProvider theme={appTheme}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </ThemeProvider>
-)
+);
